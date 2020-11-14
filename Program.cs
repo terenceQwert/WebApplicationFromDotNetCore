ccbcac014e829aaee01675191375211c1d70c48a
Terence Liu <s8854086@gmail.com>
2020-11-14 14:55:03 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+using System.Collections.Generic;
+
+namespace console {
+    class Program {
+        private static HttpClient client = new HttpClient();        
+        static void Main (string[] args) {
+            Console.WriteLine ("Hello World!");
+            //            ProcessRepository().Wait();
+            var result = ProcessRepositories().Result;
+            foreach( var repo in result)
+            {
+                Console.WriteLine("-------");
+                Console.WriteLine("{0}-{1}\n{2}",
+                    repo.Name
+                    ,repo.Description
+                    ,repo.HomePage);
+                Console.WriteLine("-------");
+            }
+        }
+        public static async Task ProcessRepository () {
+            var serializer = new DataContractJsonSerializer(typeof(List<Repository>));
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+//            var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = serializer.ReadObject(await streamTask) as List<Repository>;
+//            var msg = await stringTask;
+//            Console.WriteLine(msg);
+            foreach( var repo in repositories)
+                Console.WriteLine(repo.Name);
+        }
+        public static async Task<List<Repository>> ProcessRepositories()
+        {
+            var serializer = new DataContractJsonSerializer(typeof(List<Repository>));
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = serializer.ReadObject(await streamTask) as List<Repository>;
+            return repositories;
+        }
+    }
+}
---
+++ repo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace console
+{
+    [DataContract(Name ="repo")]    
+    public class Repository
+    {
+        [DataMember(Name = "name")]
+        public string Name {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        private string _name;
+        [DataMember(Name ="description")]
+        public string Description { get; set; }
+        
+        [DataMember(Name ="homepage")]
+        public Uri HomePage { get; set; }
+    }
+}
